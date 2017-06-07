92879af84ee241fb6abf2e67738123e9088b0c29
kadai0308 <kadai0308@gmail.com>
2017-06-07 14:57:09 +0800

    use generator to parse url - counting

--- counting/counting.py
+++ counting/counting.py
@@ -2,21 +2,25 @@
 from collections import Counter
 
 urls = [
-    "http://www.google.com/a.txt",
-    "http://www.google.com/a.txt",
-    "http://www.google.com/c.jpg",
-    "http://www.google.com/a.txt",
-    "http://www.google.com/b.txt",
-    "http://www.google.com/b.txt",
-    "http://www.google.com/c.jpg",
-    "http://www.google.com/haha.png",
+    "http://www.aa.com/a.txt",
+    "http://www.bb.com/a.txt",
+    "http://www.cc.com/c.jpg",
+    "http://www.ss.com/a.txt",
+    "http://www.ff.com/b.txt",
+    "http://www.xx.com/b.txt",
+    "http://www.ee.com/c.jpg",
+    "http://www.ww.com/haha.png",
 ]
 
-urls_count = Counter(urls)
+def urlsG(urls):
+  for url in urls:
+    yield url.split('/')[-1]
+
+urls_count = Counter(urlsG(urls))
 
 top_three = urls_count.most_common(3)
 
-top_three.sort(key=lambda s: s[0].split('/')[-1])
+top_three.sort(key=lambda s: s[0])
 
 for i in top_three:
-    print(i[0].split('/')[-1], i[1])
+    print(i[0], i[1])
